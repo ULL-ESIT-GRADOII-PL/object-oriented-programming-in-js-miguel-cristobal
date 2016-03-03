224ae6fe8ad9ba4f025b5e806da0730ceba633e8
alu0100886870 <alu0100886870@ull.edu.es>
2016-03-03 01:42:07 +0000

    Arreglo de fallos en el JS

--- assets/js/conversor.js
+++ assets/js/conversor.js
@@ -7,25 +7,36 @@
     /* ademas de new Medida(45.2, "Km") */
 	this.valor = valor;
 	this.tipo = tipo;
-	
   }
   
   function Temperatura(valor,tipo)
   {
     /* tipo es opcional. Debería admitir new Medida("45.2 F") */
 	Medida.call(this, valor, tipo);
+	//No specific prop.
   }
+  
+  Temperatura.prototype = Object.create(Medida.prototype); //Avoid new
+  Temperatura.prototype.constructor = Medida;
   
   function Celsius(valor)
   {
+	  Temperatura.call(this, valor, 'c');
   }
+  
+  Celsius.prototype = Object.create(Temperatura.prototype); //Heir
+  Celsius.prototype.constructor = Temperatura;
   
   Celsius.prototype.toFarenheit = function() {
 	  //Pasar de Celsius a Farenheit
+	  var resultado = (this.valor*1.8 + 32);
+	  return resultado;
   }
   
   Celsius.prototype.toKelvin = function() {
 	  //Pasar de Celsius a Kelvin
+	  var resultado = (this.valor + 273.15);
+	  return resultado;
   }
   
   function Farenheit(valor)
@@ -57,6 +68,10 @@
           break;
         case 'f':
           var farenheit = new Farenheit(numero);
+          elemento.innerHTML = farenheit.toCelsius().toFixed(2) + " Celsius";
+          break;
+		case 'k':
+          var kelvin = new Kelvin(numero);
           elemento.innerHTML = farenheit.toCelsius().toFixed(2) + " Celsius";
           break;
         
